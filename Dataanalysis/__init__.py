c3115d94e54b89665c8d0e0d57b2b1fcde090a3a
HUGO LIU <746384608@qq.com>
2023-03-09 14:35:31 +0800

    clear the logs and set the exp to 'Opt. DUMBO's Predicate'

--- Dataanalysis/__init__.py
+++ Dataanalysis/__init__.py
@@ -1,31 +1,31 @@
 import csv
 
 if __name__ == '__main__':
-    header = ['batch size(TXs)',
+    header = ['# nodes',
+              '# corrupted',
               'total transactions(TXs)',
               'total delay(s)',
               'average delay(s/round)',
               'average throughout(TXs/s)',
-              '5% longest delay(s)',
-              '5% shortest delay(s)',
-              '5% least throughout(TXs/s)',
-              '5% largest throughout(TXs/s)']
+              '1% longest delay(s)',
+              '1% shortest delay(s)',
+              '1% least throughout(TXs/s)',
+              '1% largest throughout(TXs/s)']
     data_set = []
+    node_set = [16, 25, 36, 49, 64]     # the number of nodes emerged in the consensus
+    corrupted_set = [5, 8, 11, 16, 21]  # the number of corrupted nodes
+    idx = 0
+    num_round = 100
+    batch_size = 5000
     with open('../log/consensus-node-0.log') as f:
         print('loading data...')
         data = f.readlines()
         round_throughout = []
         round_delay = []
         single_data = None
-        batch_size = 0
-        num_round = 0
         for line in data:
             words = line.split(' ')
-            if words[0] == "./run_local_network_test.sh":
-                print(f'=========\nbatch size: {words[3]}')
-                batch_size = int(words[3])
-                num_round = int(words[4])
-            elif words[2] == "dumbo.py" and words[8] == "Delivers":
+            if words[2] == "dumbo.py" and words[8] == "Delivers":
                 single_data = float(words[16])
             elif words[2] == "dumbo.py" and words[6] == "ACS":
                 round_delay.append(float(words[12]))
@@ -36,26 +36,28 @@ if __name__ == '__main__':
                 length = len(round_throughout)
                 round_delay.sort()
                 round_throughout.sort()
-                print(f'5% longest delay: {sum(round_delay[length-6:])/6} sec, '
-                      f'5% shortest delay: {sum(round_delay[:5])/6} sec')
-                print(f'5% least throughout: {sum(round_throughout[:5])/6} TXs/sec, '
-                      f'5% largest throughout: {sum(round_throughout[length-6:])/6} TXs/sec')
+                print(f'1% longest delay: {sum(round_delay[length-6:])/6} sec, '
+                      f'1% shortest delay: {sum(round_delay[:5])/6} sec')
+                print(f'1% least throughout: {sum(round_throughout[:5])/6} TXs/sec, '
+                      f'1% largest throughout: {sum(round_throughout[length-6:])/6} TXs/sec')
                 data_set.append({
-                    'batch size(TXs)': batch_size,
+                    '# nodes': node_set[idx],
+                    '# corrupted': corrupted_set[idx],
                     'total transactions(TXs)': float(words[16]),
                     'total delay(s)': float(words[10]),
                     'average delay(s/round)': float(words[10])/num_round,
                     'average throughout(TXs/s)': float(words[16])/float(words[10]),
-                    '5% longest delay(s)': sum(round_delay[length-6:])/6,
-                    '5% shortest delay(s)': sum(round_delay[:5])/6,
-                    '5% least throughout(TXs/s)': sum(round_throughout[:5])/6,
-                    '5% largest throughout(TXs/s)': sum(round_throughout[length-6:])/6
+                    '1% longest delay(s)': sum(round_delay[length-6:])/6,
+                    '1% shortest delay(s)': sum(round_delay[:5])/6,
+                    '1% least throughout(TXs/s)': sum(round_throughout[:5])/6,
+                    '1% largest throughout(TXs/s)': sum(round_throughout[length-6:])/6
                 })
                 round_throughout = []
                 round_delay = []
+                idx += 1
 
     print(data_set)
-    with open('data_11.csv', 'a', newline='', encoding='utf-8') as out:
+    with open('data_origin_dumbo.csv', 'a', newline='', encoding='utf-8') as out:
         writer = csv.DictWriter(out, fieldnames=header)
         writer.writeheader()
         writer.writerows(data_set)
